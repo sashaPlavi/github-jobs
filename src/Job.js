3eeac913531bcf5b329702e00930ecb6ed612e80
SashaPlavi <sasha.bingulac@gmail.com>
2021-01-10 16:54:20 +0100

    rendering cards

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Badge } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
+import { Button, Collapse } from 'react-bootstrap';
 
 export const Job = ({ job }) => {
+  const [open, setOpen] = useState(false);
+
+  const togleOpen = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
   return (
     <div>
-      <Card>
+      <Card className="mb-3">
         <Card.Body>
           <div class="d-flex justify-content-between">
             <div>
@@ -14,8 +21,34 @@ export const Job = ({ job }) => {
                   {job.company}
                 </span>
               </Card.Title>
+              <Card.Subtitle className="text-muted mb-2">
+                {new Date(job.created_at).toLocaleDateString()}
+              </Card.Subtitle>
+              <Badge variant="secondary" className="mr-2">
+                {job.type}
+              </Badge>
+              <Badge variant="secondary">{job.location}</Badge>
+              <div style={{ wordBreak: 'break-all' }}>
+                <ReactMarkdown source={job.how_to_apply} />
+              </div>
             </div>
+            <img
+              className="d-none d-md-block"
+              height="50"
+              src={job.company_logo}
+              alt={job.company}
+            />
           </div>
+          <Card.Text>
+            <Button onClick={togleOpen} variant="primary">
+              {open ? 'Hide Details' : 'View Details'}
+            </Button>
+          </Card.Text>
+          <Collapse in={open}>
+            <div class="mt-4">
+              <ReactMarkdown source={job.description} />
+            </div>
+          </Collapse>
         </Card.Body>
       </Card>
     </div>
